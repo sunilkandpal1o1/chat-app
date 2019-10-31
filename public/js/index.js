63f5399bb485259989a542273f0d619c64f09bcf
Sunil Kandpal <sskandpal.uk@gmail.com>
2019-11-01 00:28:03 +0530

    Add form and enbaled rendering of new messages

--- public/js/index.js
+++ public/js/index.js
@@ -5,9 +5,23 @@ socket.on('connect', function () {
 
 socket.on('newMessage', function (message) {
 	console.log('New message ', message);
-});
+	let li = $('<li></li>');
+	li.text(`${message.from}: ${message.text}`);
 
+	$('#message-area').append(li);
+});
 
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
+});
+
+$('#message-form').on('submit', function (e) {
+	e.preventDefault();
+
+	socket.emit('createMessage', {
+		from: 'User',
+		text: $('[name=message]').val(),
+	}, function () {
+
+	});
 });
