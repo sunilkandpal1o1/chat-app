571538df16d96ce524b870bfda07d2f3d041e497
Sunil Kandpal <sskandpal.uk@gmail.com>
2019-10-31 15:03:59 +0530

    newMessage event emit on createMessage

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+const socket = io();
+socket.on('connect', function () {
+    console.log('Connected to server');
+});
+
+socket.on('newMessage', function (message) {
+	console.log('New message ', message);
+});
+
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
--- server/server.js
+++ server/server.js
@@ -20,6 +20,22 @@ server.listen(port, ()=>{
 io.on('connection', ( socket ) => {
 	console.log('New user connected');
 
+	socket.on('createMessage', ( message ) => {
+		console.log('Created message ',message);
+
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime(),
+		});
+
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime(),
+		// });
+	});
+
 	socket.on('disconnect', () => {
 		console.log('User Disconnected');
 	});
